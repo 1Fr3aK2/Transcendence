3d8fcf3c08bc6a83466ba9c4ddfc4b1e40916be7
SuVao <pedro.radon@gmail.com>
2026-07-28 14:16:55 +0100

    oopional email in the login

--- backend/src/auth/login.dto.ts
+++ backend/src/auth/login.dto.ts
@@ -1,8 +1,12 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsOptional, IsString } from 'class-validator';
 
 export class LoginDto {
   @IsString()
   username: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
 
   @IsString()
   password: string;
