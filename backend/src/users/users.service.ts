28464fb922023bc79dbddb6c4d56775ef4eceda2
Rafael Matos <rafael.joao.amorim.matos@gmail.com>
2026-07-29 22:38:01 +0100

    donne with register and showing encrypted passwords

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -10,8 +10,20 @@ export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
   async findAll() {
-    return this.prisma.user.findMany();
-  }
+  return this.prisma.user.findMany({
+    select: {
+      id: true,
+      username: true,
+      email: true,
+      avatar: true,
+      wallet: true,
+      wins: true,
+      losses: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+}
 
   async create(createUserDto: CreateUserDto) {
     const hash = await bcrypt.hash(createUserDto.password, 10);
